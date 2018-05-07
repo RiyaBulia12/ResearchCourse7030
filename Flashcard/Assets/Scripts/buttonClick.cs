efaf5b892b41ef740ca984d573a943070844d6fd
RiyaBulia12 <riyabulia12@gmail.com>
2018-05-07 18:09:09 -0400

    FlashCard

---
+++ Flashcard/Assets/Scripts/buttonClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonClick : MonoBehaviour {
+	public cards againGO;
+	public void MainMenu ()
+	{
+		SceneManager.LoadScene ("Menu");
+	}
+
+	public void PlayAgain ()
+	{
+		SceneManager.LoadScene ("MemoryGame");
+	}
+
+}
---
+++ Flashcard/Assets/Scripts/draw.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class draw : MonoBehaviour {
+
+	public float depth = 10.0f;
+	public Transform pencil;
+
+	// Use this for initialization
+	void  Start (){
+		Cursor.visible = true;
+	}
+
+	// Update is called once per frame
+	void  Update (){
+		transform.position= Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x,Input.mousePosition.y, depth));
+	}
+
+	void  FixedUpdate (){
+		if(Input.GetMouseButtonDown(0)){
+			Instantiate (pencil, transform.position, transform.rotation);
+		}
+	}
+}
